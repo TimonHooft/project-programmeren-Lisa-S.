c56ae29f400a4cc1e52fd5346e87835791bf4739
LisaSchockaert <57811141+LisaSchockaert@users.noreply.github.com>
2019-11-15 19:57:39 +0100

    Update opdracht1.py

--- opdracht1.py
+++ opdracht1.py
@@ -5,3 +5,16 @@ Created on Fri Nov 15 19:34:01 2019
 @author: schoc
 """
 
+sequentie = "" 
+
+while True: 
+    temperatuur = input("Geef een temperatuur: ")
+    if temperatuur == "STOP": 
+        break 
+    if 0 <= float(temperatuur) <= 79 :
+        getal = float(temperatuur) / 10 
+        getal = int(getal)
+        getal = str(getal)
+        sequentie = sequentie + getal 
+
+print(" De sequentie is ", sequentie)
